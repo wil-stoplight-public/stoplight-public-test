6fe77d444d97a6bd9f1b133092cc5fbf87a09c4d
wil.kirwan@stoplight.io <wil.kirwan@stoplight.io>
2023-03-15 15:55:03 -0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MDY3Mw/spectral.js?branch_slug=master&token=98f84c0e-f297-40ef-ab9e-421cc78da0f2";
+export default { extends: ruleset };
